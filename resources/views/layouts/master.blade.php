3a9b3aea2f5721b70c79068a64f3299b67b34c67
Harry Ermawan <harryermawan@gmail.com>
2017-12-13 01:25:19 +0700

    init

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,23 @@
+
+  
+
+
+
+  <!DOCTYPE html>
+<html>
+    <head>
+        <title>@yield('title')</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+		<!-- Compiled and minified CSS -->
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+       
+    </head>
+    <body>
+        <div class="container">
+        	@yield('content')
+        </div>
+          <!-- Compiled and minified JavaScript -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+    </body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.master')
+
+@section('title')
+	Welcome!
+@endsection
+
+
+@section('content')
+	Content!
+@endsection
